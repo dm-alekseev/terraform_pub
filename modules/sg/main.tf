695d7b65f85fde742e0420141904258b2e4c5f18
test <alekseev@stw.by>
2025-03-31 19:09:26 +0300

    modified:   modules/sg/main.tf

--- modules/sg/main.tf
+++ modules/sg/main.tf
@@ -75,6 +75,12 @@ resource "aws_security_group" "rds_sg" {
     from_port       = 5432
     to_port         = 5432
     protocol        = "tcp"
-    #security_groups = [aws_security_group.private.id]
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
